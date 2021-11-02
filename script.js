4e089a5c8e2135f6d6437f3d79cc81db524ce559
mariaassisf <90994184+mariaassisf@users.noreply.github.com>
2021-11-02 19:11:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("ola seja bem vindo!")
+let nome = prompt("qual e o seu nome?")
+let ano_nascimento = parseInt(prompt("em que ano você nasceu?"))
+let ano_atual = 2021
+let mensagem = ` ola seu nome e ${nome} e sua idade e ${ano_atual-ano_nascimento}`
+alert(mensagem)
+
+
+
